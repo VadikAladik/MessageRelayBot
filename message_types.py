da6e9220f8471f5dffaa169ef26a70b14f5ba410
VadikAladik <107686157+VadikAladik@users.noreply.github.com>
2024-12-06 00:05:18 +0300

    Add files via upload
    Удалены нерабочие типы сообщений(скорее всего временно)
    Сделано удаление кнопки после нажатия

--- message_types.py
+++ message_types.py
@@ -2,11 +2,6 @@ unsupported_comment_types = {
     "audio": "аудиофайл",
     "voice": "голосовое сообщение",
     "sticker": "стикер",
-    "location": "геопозиция",
-    "contact": "контакт",
-    "venue": "место",
-    "dice": "игра в кости",
-    "video_note": "видео-заметка",
     "voice_note": "кружок"
 }
 
@@ -16,4 +11,4 @@ supported_comment_types = {
     "animation": "анимация"
 }
 
-requires_file_id = ['audio', 'voice', 'video_note', 'document', 'video', 'animation']
+requires_file_id = ['audio', 'voice', 'video_note', 'document', 'video', 'animation', 'sticker']
